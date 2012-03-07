6ae3406b926161bfbef4e6cd8e606c3b8f540878
Julian Togashi <juliantogashi@gmail.com>
2012-03-07 07:41:18 -0800

    removing tree

--- second_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SecondApp::Application.config.secret_token = '0e8d96da36ad6b02f00ff4e185390016ad6966338ab361761cf4f1df9c150ac4cbf1ba13279ded77277a7d7401c6d1c81f8daeb45ae880e0c7550b81c993be8e'
