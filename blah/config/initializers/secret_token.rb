19ffee4e9e49c46dca4508ace4edcb0aad5b1767
Julian Togashi <juliantogashi@gmail.com>
2012-04-05 00:15:35 -0700

    fix directories

--- blah/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blah::Application.config.secret_token = '7ca7007d4e1542cf1ed6ffabf11b0d98a1f7bcd93a70733c8e386522d24fde85b7bf7f7f0ed69308679174ec2ebe9f3884292b50a5cf88af009e634a88f5fb85'
